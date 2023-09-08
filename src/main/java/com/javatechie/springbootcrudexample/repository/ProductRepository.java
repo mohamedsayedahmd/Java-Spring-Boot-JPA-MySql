6845d2e55805d12ecb752b577007f1a6478d31fd
mohamedsayedahmd <mohamedsayedahmd@gmail.com>
2023-09-08 21:15:59 +0300

    create product repository and set application

---
+++ src/main/java/com/javatechie/springbootcrudexample/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.javatechie.springbootcrudexample.repository;
+
+import com.javatechie.springbootcrudexample.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
